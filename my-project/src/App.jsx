a5e46cece02bf2383bac36c459ad67e7f19c4b8f
HARIPRIYA-5565 <70848158+HARIPRIYA-5565@users.noreply.github.com>
2025-06-14 07:35:47 +0000

    commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {sideBarComponent} from "./sideBarComponent/Components/src" 
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-project/src/Components/sideBar.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+
+export default fucntion SideBarComponent(){
+return(
+<div>
+<h1><img src="/image/vayuzLogo.png"></img></h1>
+
+
+
+</div>
+    
+)
+
+
+
+}
